7d686b81f26425a13f8163426db49347bf3ae018
Julio Fernando da Silva Santos <1900534@SALAS.AULAS>
2019-11-01 20:28:23 -0300

    adicionando Dockerfile

--- com/faculdadeimpacta/calculadora/app_web_start.py
+++ com/faculdadeimpacta/calculadora/app_web_start.py
@@ -12,7 +12,7 @@ def index():
 
 def main():
     port = int(os.environ.get('PORT',5000))
-    app.run(hot='0.0.0.0',port=port)
+    app.run(host='0.0.0.0',port=port)
 
 if __name__=='__main__':
     main()
