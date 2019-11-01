69b82095d2e57dbf4d333f154e5efe5ee387293c
Julio Fernando da Silva Santos <1900534@SALAS.AULAS>
2019-11-01 19:43:25 -0300

    inclusao

---
+++ com/faculdadeimpacta/calculadora/app_web_start.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def index():
+    return 'Index Page'
+
+
+def main():
+    port = int(os.environ.get('PORT',5000))
+    app.run(hot='0.0.0.0',port=port)
+
+if __name__=='__main__':
+    main()
